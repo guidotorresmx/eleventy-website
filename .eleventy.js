26605617573e43dc5070d25323855caa4f98c475
Marco Guido <guido-torres@hotmail.com>
2022-03-27 21:57:20 -0400

    pages layout fixes #1

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,16 @@
+const pluginTOC = require('eleventy-plugin-toc');
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor');
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addLayoutAlias('default', 'layouts/default.pug');
-    eleventyConfig.setPugOptions({ debug: true });
+    eleventyConfig.setLibrary('md', markdownIt().use(markdownItAnchor));
+    eleventyConfig.addPlugin(pluginTOC);
+    global.filters = eleventyConfig.javascriptFunctions; // magic happens here
+    eleventyConfig.setPugOptions({ // and here
+        debug: true,
+        globals: ['filters']
+    });
 
 };
