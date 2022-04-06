439a58ba82a8f323b42f767276f173250c566f9f
Marco Guido <guido-torres@hotmail.com>
2022-04-06 02:44:24 -0400

    major header nav and footer changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 const pluginTOC = require('eleventy-plugin-toc');
-const markdownIt = require('markdown-it')
+const markdownIt = require('markdown-it');
 const markdownItAnchor = require('markdown-it-anchor');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addLayoutAlias('default', 'layouts/default.pug');
+    eleventyConfig.addPassthroughCopy({ '_assets/img': 'assets/img' });
     eleventyConfig.setLibrary('md', markdownIt().use(markdownItAnchor));
     eleventyConfig.addPlugin(pluginTOC);
     global.filters = eleventyConfig.javascriptFunctions; // magic happens here
